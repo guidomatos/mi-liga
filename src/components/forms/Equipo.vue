651b5b2a0776662e9f0f22bd618ed21fa4474c3a
Guido Matos Camones <guido.matos.88@gmail.com>
2020-07-31 00:56:50 -0500

    Creación de un formulario para agregar equipos

--- src/components/forms/Equipo.vue
+++ src/components/forms/Equipo.vue
@@ -1,13 +1,48 @@
 <template>
     <b-modal id="myModal" title="Nuevo Equipo" footer-class="mymodal-footer">
-      <div slot="modal-footer">
-      </div>
+
+        <b-form v-if="show">
+            <b-form-group>
+                <b-form-input v-model="form.nombre" required placeholder="Nombre de equipo" />
+            </b-form-group>
+            <b-form-group>
+                <b-form-input v-model="form.logo" required placeholder="Escribe la url" />
+            </b-form-group>
+            <b-form-group>
+                <b-form-select :options="jugadores" required multiple v-model="form.jugadores">
+                </b-form-select>
+            </b-form-group>
+            <b-form-group>
+                <b-form-radio-group v-model="form.estado">
+                    <b-form-radio value="false">Inactivo</b-form-radio>
+                    <b-form-radio value="true">Activo</b-form-radio>
+                </b-form-radio-group>
+            </b-form-group>
+        </b-form>
+
+        <div slot="modal-footer">
+        </div>
     </b-modal>
 </template>
 <script>
 export default {
     data() {
-        return {}
+        return {
+            form: {
+                nombre: "",
+                logo: "",
+                jugadores: [],
+                estado: false
+            },
+            jugadores: [
+                "Jugador 1",
+                "Jugador 2",
+                "Jugador 3",
+                "Jugador 4",
+                "Jugador 5",
+            ],
+            show: true
+        }
     }
 }
 </script>
