ee5016f0fe79a5d5b1ed15fe82b2e830ba5664ab
Guido Matos Camones <guido.matos.88@gmail.com>
2020-07-31 18:06:42 -0500

    Limpiar campos de formulario en Vue.js

--- src/components/forms/Equipo.vue
+++ src/components/forms/Equipo.vue
@@ -1,14 +1,16 @@
 <template>
-    <b-modal id="myModal" title="Nuevo Equipo" footer-class="mymodal-footer">
+    <b-modal id="myModal" title="Nuevo Equipo" 
+    body-class="mymodal-body"
+    footer-class="mymodal-footer">
 
-        <b-form v-if="show">
-            <b-form-group>
+        <b-form v-if="show" @reset="onReset">
+            <b-form-group label="Nombre:" description="Escribe el nombre del equipo">
                 <b-form-input v-model="form.nombre" required placeholder="Nombre de equipo" />
             </b-form-group>
-            <b-form-group>
+            <b-form-group label="Logo:" description="Escribe la url del logo">
                 <b-form-input v-model="form.logo" required placeholder="Escribe la url" />
             </b-form-group>
-            <b-form-group>
+            <b-form-group label="Jugadores:" description="Selecciona un jugador">
                 <b-form-select :options="jugadores" required multiple v-model="form.jugadores">
                 </b-form-select>
             </b-form-group>
@@ -18,6 +20,12 @@
                     <b-form-radio value="true">Activo</b-form-radio>
                 </b-form-radio-group>
             </b-form-group>
+
+            <div class="form-actions float-right">
+                <b-button type="reset" variant="danger">
+                    Limpiar
+                </b-button>
+            </div>
         </b-form>
 
         <div slot="modal-footer">
@@ -43,10 +51,32 @@ export default {
             ],
             show: true
         }
+    },
+    methods: {
+        onReset(evt){
+
+            evt.preventDefault();
+
+            this.form.nombre = "";
+            this.form.logo = "";
+            this.form.jugadores = [];
+            this.form.estado = false;
+
+            this.show = false;
+            this.$nextTick(() => {
+                this.show = true;
+            });
+        }
     }
 }
 </script>
 
-<style scoped>
+<style>
+.mymodal-body .col-form-label {
+    text-align: left;
+}
 
+.mymodal-body .text-muted {
+    text-align: right;
+}
 </style>
